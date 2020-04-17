42381e05dc768cbb904c996aea11380c3c88a93a
Paweł Wicher <pwi@epcon.pl>
2020-04-17 08:54:49 +0200

    cleanup

--- src/Main.elm
+++ src/Main.elm
@@ -2,12 +2,8 @@ module Main exposing (main)
 
 import Browser
 import Html exposing (..)
--- START:imports
-import Html.Attributes
-    exposing
-        ( class, disabled, placeholder, src, type_, value )
+import Html.Attributes exposing (class, disabled, placeholder, src, type_, value)
 import Html.Events exposing (onClick, onInput, onSubmit)
--- END:imports
 import Browser.Events exposing (onKeyDown)
 import Json.Decode as Decode
 
@@ -19,35 +15,17 @@ type alias Model =
     , newComment : String
     }
 
-
 baseUrl : String
-baseUrl =
-    "https://programming-elm.com/"
-
-keyDecoder : Decode.Decoder Msg
-keyDecoder =
-  Decode.map toKey (Decode.field "key" Decode.string)
-
-toKey : String -> Msg
-toKey keyValue =
-  case keyValue of
-    "ArrowUp" ->
-      ToggleLike
-    "ArrowDown" ->
-      ToggleLike
-    _ ->
-      ToggleLike
-
+baseUrl = "https://programming-elm.com/"
 
 initialModel : Model
 initialModel =
     { url = baseUrl ++ "1.jpg"
     , caption = "Surfing"
     , liked = False
-    , comments = [ "Cowabunga, dude!" ]
+    , comments = [ "Cowabunga, dude" ]
     , newComment = ""
     }
-
 
 viewLoveButton : Model -> Html Msg
 viewLoveButton model =
@@ -68,14 +46,12 @@ viewLoveButton model =
             []
         ]
 
-
 viewComment : String -> Html Msg
 viewComment comment =
     li []
         [ strong [] [ text "Comment:" ]
         , text (" " ++ comment)
         ]
-
 
 viewCommentList : List String -> Html Msg
 viewCommentList comments =
@@ -89,27 +65,23 @@ viewCommentList comments =
                     (List.map viewComment comments)
                 ]
 
-
 viewComments : Model -> Html Msg
--- START:viewComments
 viewComments model =
     div []
         [ viewCommentList model.comments
-        , form [ class "new-comment", onSubmit SaveComment ] -- (1)
+        , form [ class "new-comment", onSubmit SaveComment ]
             [ input
                 [ type_ "text"
                 , placeholder "Add a comment..."
-                , value model.newComment -- (2)
-                , onInput UpdateComment -- (3)
+                , value model.newComment
+                , onInput UpdateComment
                 ]
                 []
             , button
-                [ disabled (String.isEmpty model.newComment) ] -- (4)
+                [ disabled (String.isEmpty model.newComment) ]
                 [ text "Save" ]
             ]
         ]
--- END:viewComments
-
 
 viewDetailedPhoto : Model -> Html Msg
 viewDetailedPhoto model =
@@ -122,11 +94,6 @@ viewDetailedPhoto model =
             ]
         ]
 
-init : flags -> ( Model, Cmd msg )
-init _ =
-    ( initialModel, Cmd.none )
-
-
 view : Model -> Html Msg
 view model =
     div []
@@ -136,16 +103,11 @@ view model =
             [ viewDetailedPhoto model ]
         ]
 
-
--- START:msg
 type Msg
     = ToggleLike
     | UpdateComment String
     | SaveComment
--- END:msg
 
-
--- START:saveNewComment
 saveNewComment : Model -> Model
 saveNewComment model =
     let
@@ -161,32 +123,24 @@ saveNewComment model =
                 | comments = model.comments ++ [ comment ]
                 , newComment = ""
             }
--- END:saveNewComment
 
-
-update : Msg -> Model -> ( Model, Cmd Msg )
--- START:update
+update : Msg -> Model -> Model
 update msg model =
     case msg of
         ToggleLike ->
-            ( { model | liked = not model.liked }, Cmd.none )
+            { model | liked = not model.liked }
 
         UpdateComment comment ->
-            ( { model | newComment = comment }, Cmd.none )
+            { model | newComment = comment }
 
         SaveComment ->
-            ( saveNewComment model, Cmd.none )
--- END:update
+            saveNewComment model
 
-subscriptions : Model -> Sub Msg
-subscriptions _ =
-    onKeyDown keyDecoder
 
 main : Program () Model Msg
 main =
-    Browser.element
-        { init = init
+    Browser.sandbox
+        { init = initialModel
         , view = view
         , update = update
-        , subscriptions = subscriptions
         }
