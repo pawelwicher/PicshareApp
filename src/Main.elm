b68d9c3b3dff8452267bedcb85246367cebfa020
Paweł Wicher <pwi@epcon.pl>
2020-11-30 17:55:42 +0100

    Picshare app cleaning

--- src/Main.elm
+++ src/Main.elm
@@ -6,139 +6,133 @@ import Html.Attributes exposing (class, disabled, placeholder, src, type_, value
 import Html.Events exposing (onClick, onInput, onSubmit)
 
 type alias Model =
-    { url : String
-    , caption : String
-    , liked : Bool
-    , comments : List String
-    , newComment : String
-    }
+  { url : String
+  , caption : String
+  , liked : Bool
+  , comments : List String
+  , newComment : String
+  }
 
 baseUrl : String
-baseUrl = "https://programming-elm.com/"
+baseUrl =
+  "https://programming-elm.com/"
 
 initialModel : Model
 initialModel =
-    { url = baseUrl ++ "1.jpg"
-    , caption = "Surfing"
-    , liked = False
-    , comments = [ "Cowabunga, dude" ]
-    , newComment = ""
-    }
+  { url = baseUrl ++ "1.jpg"
+  , caption = "Surfing"
+  , liked = False
+  , comments = [ "Cowabunga, dude!" ]
+  , newComment = ""
+  }
 
 viewLoveButton : Model -> Html Msg
 viewLoveButton model =
-    let
-        buttonClass =
-            if model.liked then
-                "fa-heart"
-
-            else
-                "fa-heart-o"
-    in
-    div [ class "like-button" ]
-        [ i
-            [ class "fa fa-2x"
-            , class buttonClass
-            , onClick ToggleLike
-            ]
-            []
-        ]
+  let
+    buttonClass =
+      if model.liked then
+        "fa-heart"
+      else
+        "fa-heart-o"
+  in
+  div [ class "like-button" ]
+    [ i
+      [ class "fa fa-2x"
+      , class buttonClass
+      , onClick ToggleLike
+      ]
+      []
+    ]
 
 viewComment : String -> Html Msg
 viewComment comment =
-    li []
-        [ strong [] [ text "Comment:" ]
-        , text (" " ++ comment)
-        ]
+  li []
+    [ strong [] [ text "Comment:" ]
+    , text (" " ++ comment)
+    ]
 
 viewCommentList : List String -> Html Msg
 viewCommentList comments =
-    case comments of
-        [] ->
-            text ""
-
-        _ ->
-            div [ class "comments" ]
-                [ ul []
-                    (List.map viewComment comments)
-                ]
+  case comments of
+    [] ->
+      text ""
+    _ ->
+      div [ class "comments" ]
+        [ ul []
+          (List.map viewComment comments)
+        ]
 
 viewComments : Model -> Html Msg
 viewComments model =
-    div []
-        [ viewCommentList model.comments
-        , form [ class "new-comment", onSubmit SaveComment ]
-            [ input
-                [ type_ "text"
-                , placeholder "Add a comment..."
-                , value model.newComment
-                , onInput UpdateComment
-                ]
-                []
-            , button
-                [ disabled (String.isEmpty model.newComment) ]
-                [ text "Save" ]
-            ]
+  div []
+    [ viewCommentList model.comments
+    , form [ class "new-comment", onSubmit SaveComment ]
+      [ input
+        [ type_ "text"
+        , placeholder "Add a comment..."
+        , value model.newComment
+        , onInput UpdateComment
         ]
+        []
+      , button
+        [ disabled (String.isEmpty model.newComment) ]
+        [ text "Save" ]
+      ]
+    ]
 
 viewDetailedPhoto : Model -> Html Msg
 viewDetailedPhoto model =
-    div [ class "detailed-photo" ]
-        [ img [ src model.url ] []
-        , div [ class "photo-info" ]
-            [ viewLoveButton model
-            , h2 [ class "caption" ] [ text model.caption ]
-            , viewComments model
-            ]
-        ]
+  div [ class "detailed-photo" ]
+    [ img [ src model.url ] []
+    , div [ class "photo-info" ]
+      [ viewLoveButton model
+      , h2 [ class "caption" ] [ text model.caption ]
+      , viewComments model
+      ]
+    ]
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ div [ class "header" ]
-            [ h1 [] [ text "Picshare" ] ]
-        , div [ class "content-flow" ]
-            [ viewDetailedPhoto model ]
-        ]
+  div []
+    [ div [ class "header" ]
+      [ h1 [] [ text "Picshare" ] ]
+    , div [ class "content-flow" ]
+      [ viewDetailedPhoto model ]
+    ]
 
 type Msg
-    = ToggleLike
-    | UpdateComment String
-    | SaveComment
+  = ToggleLike
+  | UpdateComment String
+  | SaveComment
 
 saveNewComment : Model -> Model
 saveNewComment model =
-    let
-        comment =
-            String.trim model.newComment
-    in
-    case comment of
-        "" ->
-            model
-
-        _ ->
-            { model
-                | comments = model.comments ++ [ comment ]
-                , newComment = ""
-            }
+  let
+    comment = String.trim model.newComment
+  in
+  case comment of
+    "" ->
+      model
+    _ ->
+      { model
+        | comments = model.comments ++ [ comment ]
+        , newComment = ""
+      }
 
 update : Msg -> Model -> Model
 update msg model =
-    case msg of
-        ToggleLike ->
-            { model | liked = not model.liked }
-
-        UpdateComment comment ->
-            { model | newComment = comment }
-
-        SaveComment ->
-            saveNewComment model
-
+  case msg of
+    ToggleLike ->
+      { model | liked = not model.liked }
+    UpdateComment comment ->
+      { model | newComment = comment }
+    SaveComment ->
+      saveNewComment model
 
 main : Program () Model Msg
 main =
-    Browser.sandbox
-        { init = initialModel
-        , view = view
-        , update = update
-        }
+  Browser.sandbox
+    { init = initialModel
+    , view = view
+    , update = update
+    }
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -5,17 +5,14 @@ import Expect
 
 all : Test
 all =
-    describe "A Test Suite"
-        [ test "Addition" <|
-            \_ ->
-                Expect.equal 10 (3 + 7)
-        , test "String.left" <|
-            \_ ->
-                Expect.equal "a" (String.left 1 "abcdefg")
-        , test "String.left 2" <|
-            \_ ->
-                Expect.equal "ab" (String.left 2 "abcdefg")
-        , test "This test should fail" <|
-            \_ ->
-                Expect.fail "failed as expected!"
-        ]
+  describe "A Test Suite"
+    [ test "Addition" <|
+      \_ ->
+        Expect.equal 10 (3 + 7)
+    , test "String.left" <|
+      \_ ->
+        Expect.equal "a" (String.left 1 "abcdefg")
+    , test "This test should fail" <|
+      \_ ->
+        Expect.fail "failed as expected!"
+    ]
